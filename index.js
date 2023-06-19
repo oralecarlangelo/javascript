6606a9ad5f610c89182cd1c069a2903460a8606c
Carl Orale <oralecarlangelo@gmail.com>
2023-06-20 06:31:41 +0800

    feat: data types code.

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
-// Variable declaration using var, let, and const
-var name = "John";  // Mutable variable using var
-let age = 25;      // Mutable variable using let
-const PI = 3.1415; // Immutable variable using const
+// Numbers
+const age = 25;
+const price = 9.99;
+console.log(age); // Output: 25
+console.log(price); // Output: 9.99
 
-// Modifying variable values
-name = "Jane";
-age = 30;
+// Strings
+const name = "John Doe";
+const message = "Hello, world!";
+console.log(name); // Output: John Doe
+console.log(message); // Output: Hello, world!
 
-console.log(name); // Output: Jane
-console.log(age);  // Output: 30
-console.log(PI);   // Output: 3.1415
+// Booleans
+const isActive = true;
+const isLoggedIn = false;
+console.log(isActive); // Output: true
+console.log(isLoggedIn); // Output: false
 
-var fruit = "apple";
-let quantity = 5;
+// Null and Undefined
+let variable1 = null;
+let variable2; // undefined
+console.log(variable1); // Output: null
+console.log(variable2); // Output: undefined
 
-console.log(fruit);     // Output: apple
-console.log(quantity);  // Output: 5
+// Objects
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York"
+};
+console.log(person); // Output: { name: "John", age: 30, city: "New York" }
 
-quantity *= 2;
+// Symbols
+const id = Symbol("unique identifier");
+console.log(id); // Output: Symbol(unique identifier)
 
-console.log(quantity);  // Output: 10
+// Unknown
+let value;
+value = 5; // value is now a number
+console.log(value); // Output: 5
+value = "Hello"; // value is now a string
+console.log(value); // Output: Hello
+value = true; // value is now a boolean
+console.log(value); // Output: true
+value = null; // value is now null
+console.log(value); // Output: null
+value = { name: "John" }; // value is now an object
+console.log(value); // Output: { name: "John" }
+value = Symbol("unique identifier"); // value is now a symbol
+console.log(value); // Output: Symbol(unique identifier)
+value = undefined; // value is now undefined
+console.log(value); // Output: undefined
