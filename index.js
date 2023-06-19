2e21f77971f3f2fccfec09cca724a54c29321818
Carl Orale <oralecarlangelo@gmail.com>
2023-06-20 06:17:30 +0800

    feat: created a sample for let, var and const.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("HELLO")
+// Variable declaration using var, let, and const
+var name = "John";  // Mutable variable using var
+let age = 25;      // Mutable variable using let
+const PI = 3.1415; // Immutable variable using const
+
+// Modifying variable values
+name = "Jane";
+age = 30;
+
+console.log(name); // Output: Jane
+console.log(age);  // Output: 30
+console.log(PI);   // Output: 3.1415
+
+var fruit = "apple";
+let quantity = 5;
+
+console.log(fruit);     // Output: apple
+console.log(quantity);  // Output: 5
+
+quantity *= 2;
+
+console.log(quantity);  // Output: 10
