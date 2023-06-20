b08d8b45a69b7485f8f258ddf3fd248da9aa617b
Carl Orale <oralecarlangelo@gmail.com>
2023-06-20 12:57:59 +0800

    feat: this

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-// Selection of elements
-const titleElement = document.getElementById("title");
-const contentElements = document.getElementsByClassName("content");
-const buttonElement = document.getElementById("myButton");
+// Example 1: "this" in Object Method
+const person = {
+  name: "John",
+  age: 30,
+  greet: function () {
+    console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old.`);
+  }
+};
 
-// Manipulating content
-titleElement.textContent = "Updated Title";
+person.greet(); // Output: Hello, my name is John and I'm 30 years old.
 
-for (let i = 0; i < contentElements.length; i++) {
-  contentElements[i].innerHTML = "Updated content " + (i + 1);
+// Example 2: "this" in Function Constructor
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
 }
 
-// Handling events
-buttonElement.addEventListener("click", () => {
-  console.log("Button clicked!");
-});
+Car.prototype.getDetails = function () {
+  console.log(`This car is a ${this.make} ${this.model}.`);
+};
 
-buttonElement.addEventListener("mouseover", () => {
-  console.log("Mouse over button!");
-});
+const myCar = new Car("Toyota", "Camry");
+myCar.getDetails(); // Output: This car is a Toyota Camry.
