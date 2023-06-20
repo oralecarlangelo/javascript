bb1e8adc31ba82408aa94b0e3ccc9081e3965381
Carl Orale <oralecarlangelo@gmail.com>
2023-06-20 08:46:47 +0800

    feat: control-structures

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
-// Arithmetic Operators
-let num1 = 10;
-let num2 = 5;
+// If...Else
+let age = 18;
 
-let addition = num1 + num2;
-console.log(addition); // Output: 15
+if (age >= 18) {
+  console.log("You are eligible to vote.");
+} else {
+  console.log("You are not eligible to vote.");
+}
 
-let subtraction = num1 - num2;
-console.log(subtraction); // Output: 5
+// Else If
+let time = 12;
 
-let multiplication = num1 * num2;
-console.log(multiplication); // Output: 50
+if (time < 12) {
+  console.log("Good morning!");
+} else if (time < 18) {
+  console.log("Good afternoon!");
+} else {
+  console.log("Good evening!");
+}
 
-let division = num1 / num2;
-console.log(division); // Output: 2
+// Switch
+let day = "Monday";
 
-let remainder = num1 % num2;
-console.log(remainder); // Output: 0
+switch (day) {
+  case "Monday":
+    console.log("Today is Monday.");
+    break;
+  case "Tuesday":
+    console.log("Today is Tuesday.");
+    break;
+  case "Wednesday":
+    console.log("Today is Wednesday.");
+    break;
+  default:
+    console.log("Today is some other day.");
+}
 
-// Assignment Operators
-let x = 10;
-x += 5;
-console.log(x); // Output: 15
+// For Loop
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
 
-let y = 20;
-y -= 3;
-console.log(y); // Output: 17
+// While Loop
+let count = 0;
 
-let z = 5;
-z *= 2;
-console.log(z); // Output: 10
+while (count < 5) {
+  console.log(count);
+  count++;
+}
 
-let w = 12;
-w /= 4;
-console.log(w); // Output: 3
+// Do...While Loop
+let i = 0;
 
-// Comparison Operators
-let a = 5;
-let b = 10;
-
-console.log(a === b); // Output: false
-console.log(a !== b); // Output: true
-console.log(a > b); // Output: false
-console.log(a < b); // Output: true
-console.log(a >= b); // Output: false
-console.log(a <= b); // Output: true
-
-// Logical Operators
-let p = true;
-let q = false;
-
-console.log(p && q); // Output: false
-console.log(p || q); // Output: true
-console.log(!p); // Output: false
+do {
+  console.log(i);
+  i++;
+} while (i < 5);
