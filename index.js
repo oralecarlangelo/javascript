02cf1cff8dbee9158dca6c9e3acefd95f7ddf307
Carl Orale <oralecarlangelo@gmail.com>
2023-06-20 12:38:56 +0800

    feat: desctructuring

--- index.js
+++ index.js
@@ -1,57 +1,19 @@
-// JSON Object
+// Object Destructuring
 const person = {
-  name: "John",
+  name: "John Doe",
   age: 30,
   city: "New York"
 };
 
-// Convert Object to JSON
-const personJSON = JSON.stringify(person);
-console.log(personJSON); // Output: {"name":"John","age":30,"city":"New York"}
+const { name, age, city } = person;
+console.log(name); // Output: John Doe
+console.log(age); // Output: 30
+console.log(city); // Output: New York
 
-// Convert JSON to Object
-const personObject = JSON.parse(personJSON);
-console.log(personObject); // Output: { name: "John", age: 30, city: "New York" }
+// Array Destructuring
+const numbers = [1, 2, 3, 4, 5];
 
-
-// Array of Objects
-const employees = [
-  {
-    id: 1,
-    name: "John Doe",
-    position: "Developer",
-    salary: 50000
-  },
-  {
-    id: 2,
-    name: "Jane Smith",
-    position: "Designer",
-    salary: 60000
-  },
-  {
-    id: 3,
-    name: "Bob Johnson",
-    position: "Manager",
-    salary: 80000
-  }
-];
-
-// Convert Array of Objects to JSON
-const employeesJSON = JSON.stringify(employees);
-console.log(employeesJSON);
-// Output:
-// [
-//   {"id":1,"name":"John Doe","position":"Developer","salary":50000},
-//   {"id":2,"name":"Jane Smith","position":"Designer","salary":60000},
-//   {"id":3,"name":"Bob Johnson","position":"Manager","salary":80000}
-// ]
-
-// Convert JSON to Array of Objects
-const employeesArray = JSON.parse(employeesJSON);
-console.log(employeesArray);
-// Output:
-// [
-//   { id: 1, name: 'John Doe', position: 'Developer', salary: 50000 },
-//   { id: 2, name: 'Jane Smith', position: 'Designer', salary: 60000 },
-//   { id: 3, name: 'Bob Johnson', position: 'Manager', salary: 80000 }
-// ]
+const [first, second, , fourth] = numbers;
+console.log(first); // Output: 1
+console.log(second); // Output: 2
+console.log(fourth); // Output: 4
