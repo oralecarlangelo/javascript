075b95cb5bf929af39f16e30b4990b4d0cd05756
Carl Orale <oralecarlangelo@gmail.com>
2023-06-20 11:42:54 +0800

    fix: remove array functions

--- index.js
+++ index.js
@@ -24,39 +24,3 @@ console.log(fruits); // Output: ["apple", "banana", "orange"]
 numbers.forEach((num) => {
   console.log(num);
 });
-
-// Creating a new Array with map()
-const doubledNumbers = numbers.map((num) => num * 2);
-console.log(doubledNumbers); // Output: [2, 4, 20, 8, 10]
-
-// Filtering Array Elements with filter()
-const filteredNumbers = numbers.filter((num) => num > 3);
-console.log(filteredNumbers); // Output: [4, 5]
-
-// Checking if Array Elements satisfy a condition with every()
-const allPositive = numbers.every((num) => num > 0);
-console.log(allPositive); // Output: true
-
-// Checking if at least one Array Element satisfies a condition with some()
-const hasNegative = numbers.some((num) => num < 0);
-console.log(hasNegative); // Output: false
-
-// Finding the first Array Element that satisfies a condition with find()
-const firstEven = numbers.find((num) => num % 2 === 0);
-console.log(firstEven); // Output: 2
-
-// Finding the index of the first Array Element that satisfies a condition with findIndex()
-const firstEvenIndex = numbers.findIndex((num) => num % 2 === 0);
-console.log(firstEvenIndex); // Output: 1
-
-// Creating a new Array with elements that pass a test using filter() and map()
-const filteredDoubled = numbers.filter((num) => num > 2).map((num) => num * 2);
-console.log(filteredDoubled); // Output: [20, 8, 10]
-
-// Reducing an Array to a single value with reduce()
-const sum = numbers.reduce((total, num) => total + num, 0);
-console.log(sum); // Output: 15
-
-// Checking if a value includes inside the array
-console.log(numbers.includes(1)); // Output: true
-console.log(numbers.includes(6)); // Output: false
