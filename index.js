fde73bfa71083a2f27512bf965d55e511187cd37
Carl Orale <oralecarlangelo@gmail.com>
2023-06-20 11:16:15 +0800

    feature: functions

--- index.js
+++ index.js
@@ -1,57 +1,20 @@
-// If...Else
-let age = 18;
-
-if (age >= 18) {
-  console.log("You are eligible to vote.");
-} else {
-  console.log("You are not eligible to vote.");
+// Function Declaration
+function greet(name) {
+  console.log(`Hello, ${name}!`);
 }
 
-// Else If
-let time = 12;
+greet("John"); // Output: Hello, John!
 
-if (time < 12) {
-  console.log("Good morning!");
-} else if (time < 18) {
-  console.log("Good afternoon!");
-} else {
-  console.log("Good evening!");
-}
+// Function Expression
+const greetExpression = function (name) {
+  console.log(`Hello, ${name}!`);
+};
 
-// Switch
-let day = "Monday";
+greetExpression("Jane"); // Output: Hello, Jane!
 
-switch (day) {
-  case "Monday":
-    console.log("Today is Monday.");
-    break;
-  case "Tuesday":
-    console.log("Today is Tuesday.");
-    break;
-  case "Wednesday":
-    console.log("Today is Wednesday.");
-    break;
-  default:
-    console.log("Today is some other day.");
-}
+// Arrow Function
+const greetArrow = (name) => {
+  console.log(`Hello, ${name}!`);
+};
 
-// For Loop
-for (let i = 0; i < 5; i++) {
-  console.log(i);
-}
-
-// While Loop
-let count = 0;
-
-while (count < 5) {
-  console.log(count);
-  count++;
-}
-
-// Do...While Loop
-let i = 0;
-
-do {
-  console.log(i);
-  i++;
-} while (i < 5);
+greetArrow("Alice"); // Output: Hello, Alice!
