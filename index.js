cd8c1bc0d394d1792e2f09195394f9c3d9469699
Carl Orale <oralecarlangelo@gmail.com>
2023-06-22 06:35:15 +0800

    feat: event loop

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
-// Product classes
-class ProductA {
-  constructor() {
-    this.name = 'Product A';
-  }
-  // ...
-}
+console.log('Start');
 
-class ProductB {
-  constructor() {
-    this.name = 'Product B';
-  }
-  // ...
-}
+setTimeout(() => {
+  console.log('SetTimeout');
+}, 0);
 
-// Factory class
-class Factory {
-  createProduct(type) {
-    if (type === 'A') {
-      return new ProductA();
-    } else if (type === 'B') {
-      return new ProductB();
-    } else {
-      throw new Error('Invalid product type.');
-    }
-  }
-}
+Promise.resolve().then(() => {
+  console.log('Promise');
+});
 
-// Usage
-const factory = new Factory();
-
-const productA = factory.createProduct('A');
-console.log(productA.name); // Output: Product A
-
-const productB = factory.createProduct('B');
-console.log(productB.name); // Output: Product B
+console.log('End');
