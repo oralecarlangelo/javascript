bdcdd96d82c51305180a80d0f4788ff9e4ba5752
Carl Orale <oralecarlangelo@gmail.com>
2023-06-22 06:49:29 +0800

    feat: debouncing-throttling

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-const list = document.querySelector('ul');
+function debounce(func, delay) {
+  let timeoutId;
+  
+  return function (...args) {
+    clearTimeout(timeoutId);
+    
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
 
-list.addEventListener('click', (event) => {
-  if (event.target.tagName === 'LI') {
-    event.target.style.backgroundColor = 'yellow';
-  }
-});
+function handleScroll() {
+  console.log('Handling scroll event...');
+  // Perform scroll-related tasks here
+}
 
-document.addEventListener('click', (event) => {
-  console.log('Clicked element:', event.target);
-});
+const debouncedScroll = debounce(handleScroll, 200);
+
+window.addEventListener('scroll', debouncedScroll);
+
+
+function throttle(func, interval) {
+  let lastExecutionTime = 0;
+  
+  return function (...args) {
+    const now = Date.now();
+    
+    if (now - lastExecutionTime >= interval) {
+      func.apply(this, args);
+      lastExecutionTime = now;
+    }
+  };
+}
+
+function handleResize() {
+  console.log('Handling resize event...');
+  // Perform resize-related tasks here
+}
+
+const throttledResize = throttle(handleResize, 2000);
+
+window.addEventListener('resize', throttledResize);
